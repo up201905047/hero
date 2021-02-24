a4e12d4781ad39bddc153fa2445f8c9ec51626e1
Guilherme Garrido <up201905407@fe.up.pt>
2021-02-24 11:51:35 +0000

    ex7

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,16 +1,22 @@
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.input.KeyStroke;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.IOSafeExtendedTerminal;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
     TerminalSize terminalSize;
     Screen screen;
+    private int x = 10;
+    private int y = 10;
+
     public Game(){
         try {
             terminalSize = new TerminalSize(40, 20);
@@ -31,17 +37,51 @@ public class Game {
     private void draw() throws IOException{
         //try {
             screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('G')[0]);
+            screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
             screen.refresh();
         //} catch (IOException e) {
         //    e.printStackTrace();
         //}
     }
 
-    public void run(){
-        try {
-            draw();
+    public void run() {
+        boolean ctrl = true;
+        while (ctrl) {
+            try {
+                draw();
+                KeyStroke k = screen.readInput();
+                ctrl = processKey(k);
+            } catch (IOException e) {
+            }
         }
-        catch (IOException e){}
+        try {
+            screen.close();
+        } catch (IOException e) {
+        }
+
+    }
+
+    private boolean processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case ArrowLeft:
+                x = x - 1;
+                break;
+            case ArrowRight:
+                x += 1;
+                break;
+            case ArrowUp:
+                y -= 1;
+                break;
+            case ArrowDown:
+                y += 1;
+                break;
+            default:
+        }
+        if ((key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') || key.getKeyType() == KeyType.EOF)
+            return false;
+        else
+            return true;
+
+
     }
 }
